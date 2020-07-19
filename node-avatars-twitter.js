e3783da2e32da73d15046decf009ecf7d2a5bdff
James Bateson <jim.bateson@outlook.com>
2020-07-19 16:36:22 +0100

    feat: twitter avatars (#245)
    * chore: package-lock update
    
    * chore: remove logo from footer
    
    * style: some more spacing tweaks
    
    I NEED TO STOP
    
    * chore: remove CSS sticky nav for now
    
    I think this will be useful, but need to re-think its implementation!
    
    * style: increase navigation font-size on mobile
    
    Was failing tap target lighthouse
    
    * chore: some image optimisation
    
    * chore: manual bump of package-lock dependancy
    
    * build: added requirements file for subfont plugin usage
    
    * chore: some font loading strategy changes
    
    * build: updated netlify config with font loading option
    
    * build: netlify config tweaks
    
    * build: set netlify font loading plugin to inline css
    
    * style: project listing images aspect-ratio
    
    Fixes #182
    
    * style: subtle background for intro component
    
    Not on project page due to image. Hopefully make things look a little bit less spacey in places
    
    * style: still show intro background on project page at smaller resolutions
    
    * style: some overflow scroll spacing and added a subtle intro background color
    
    Still undecided on the colour!
    
    * chore: curly single quote on articles page intro summary
    
    * chore: 11ty config spacing tweak
    
    * build: plugin error debugging
    
    * build: removed subfont plugin
    
    * chore: couple of manual deps updates
    
    * style: couple of fussy style tweaks
    
    * chore: add font pre-loading back in for fonts visible on load
    
    * style: some minor post font weight tweaks
    
    * chore: added last couple of fonts to preload
    
    * chore: optimised social sharing image
    
    * style: links in post notes
    
    * fix: manifest.json path and rollup version bump
    
    Fixes #225
    
    * style: post note background color tweak
    
    Now meets AA a11y standards
    
    * chore(deps): a few manual version bumps
    
    * feat: twitter avatars (#243)
    
    * feat: initial implementation of avatar icons script
    
    * Update Pages “resume”
    
    * Update Pages “resume”
    
    * style: resume avatar styling
    
    * style: minor tweak to light/dark toggle styling
    
    * chore: cms branch back to main (#244)
    
    * feat: initial implementation of avatar icons script
    
    * Update Pages “resume”
    
    * Update Pages “resume”
    
    * style: resume avatar styling
    
    * style: minor tweak to light/dark toggle styling
    
    * chore: cms url back to master branch
    
    Co-authored-by: James Bateson <james@fatmedia.co.uk>

---
+++ node-avatars-twitter.js
@@ -0,0 +1,54 @@
+const slugify = require("slugify");
+const fs = require("fs-extra");
+const fastglob = require("fast-glob");
+const getTwitterAvatarUrl = require("twitter-avatar-url");
+const eleventyImg = require("@11ty/eleventy-img");
+const cleanName = require("./src/config/cleanAuthorName");
+
+eleventyImg.concurrency = 5;
+getTwitterAvatarUrl.concurrency = 1;
+
+async function fetch(name) {
+	if(!name) {
+		return;
+	}
+
+	let slug = slugify(name).toLowerCase();
+	let dir = `./src/avatars/twitter/`;
+	await fs.ensureDir(dir);
+
+	let path = `${dir}${slug}.json`;
+
+	if(!fs.pathExistsSync(path)) {
+		console.log( "Getting image url for", name );
+		let result = await getTwitterAvatarUrl(name);
+		if(result && result.url.large) {
+			let stats = await eleventyImg(result.url.large, {
+				formats: ["webp", "jpeg"],
+				widths: [90],
+				urlPath: "./src/images/avatars/twitter/",
+				outputDir: "./src/images/avatars/twitter/",
+			});
+
+			return fs.writeFile(path, JSON.stringify(stats, null, 2));
+		}
+
+		console.log(`Could not retrieve twitter avatar url for ${name}`, result);
+	}
+}
+
+(async function() {
+	let twitterUsernames = new Set();
+
+	// Twitter
+	let resume = require("./src/_data/resume.json").map(entry => entry.twitter);
+	for(let twitter of resume) {
+		twitterUsernames.add(cleanName(twitter).toLowerCase());
+	}
+
+	console.log( "Found", twitterUsernames.size, "usernames" );
+	let sorted = Array.from(twitterUsernames).sort();
+	for(let name of sorted) {
+		await fetch(name);
+	}
+})();
