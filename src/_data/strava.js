3f924752a50c0042d74f9ef5b38529241e625f54
James Bateson <jim.bateson@outlook.com>
2022-04-20 15:37:57 +0100

    chore(deps): fixes for major elevently-img bump

--- src/_data/strava.js
+++ src/_data/strava.js
@@ -1,4 +1,4 @@
-const Cache = require('@11ty/eleventy-cache-assets');
+const Cache = require('@11ty/eleventy-fetch');
 const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 dotenv.config();
